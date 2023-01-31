8070f2634850a38fe6bf1ec87df31c07664ef97e
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-01-31 20:10:57 +0200

    first commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true} ));
-app.use(commentsRouter)
+app.use(commentsRouter);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
