df87cc9e8655fd575be962fcaa799f399f02045b
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-02-04 15:01:51 +0200

    prevent sql injection

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,12 +2,23 @@ import * as commentService from '../services/comments.js';
 
 export const getAllComments = async(req, res) => {
   const comments = await commentService.getAllComments();
+  if (!comments) {
+    res.sendStatus(404);
+    return;
+  }
+
   res.send(comments);
 }
 
 export const getSelectedComments = async(req, res) => {
   const {parentid} = req.params
-  const comments = await commentService.getSelectedComments(parentid)
+  const comments = await commentService.getSelectedComments(parentid);
+
+  if (!comments) {
+    res.sendStatus(404);
+    return;
+  }
+
   res.send(comments);
 };
 
